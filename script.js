d2f7629d60aa5704736175bff335934591bf294f
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2023-11-09 11:06:03 -0500

    base functionality complete. needs QR code

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// UNIVERSAL METHODS
+
+// Displays the current date and time
 const currentTimeElement = document.getElementById('current-time');
 const currentDateElement = document.getElementById('current-date');
+let studentEnd;
+let currentColor = 'darkgreen';
 
 setInterval(function () {
     const currentDate = new Date();
@@ -9,13 +14,12 @@ setInterval(function () {
     currentTimeElement.innerText = timeText;
 }, 1000);
 
-function getURLData() {
-    const thisURL = window.location.href;
-    const parameters = new URLSearchParams(thisURL);
-    const validDate = parameters.getAll('date');
-    const startTime = parameters.getAll('time');
-    const duration = parameters.getAll('duration');
-    console.log(validDate + ' - ' + startTime + ' - ' + duration);
+// END UNIVERSAL METHODS
+
+// FORM.HTML METHODS
+function updateDuration() {
+    const duration = document.getElementById('duration-input').value;
+    const durationDisplay = document.getElementById('duration-display').innerHTML = duration + ' minutes';
 }
 
 function requestPass() {
@@ -25,25 +29,67 @@ function requestPass() {
 
 
     // Check for invalid inputs
-    if (studentName == "" | studentDestination == "" | studentDuration == "") {
-        alert("Please complete all three forms before requesting a pass.");
+    if (studentName == '' | studentDestination == '' | studentDuration == '') {
+        alert('Please complete all three forms before requesting a pass.');
         return;
     }
 
-    try {
-        studentDuration = float(studentDuration);
-        studentDuration = studentDuration.toFixed();
-    } catch (error) {
-        alert("Please enter a valid whole number for your pass duration.");
-        
+    // Generate URL parameter string
+    let urlParameters = 'name=' + studentName + '&destination=' + studentDestination + '&duration=' + studentDuration;
+    const safeDate = encodeURIComponent(new Date());
+    urlParameters += '&date=' + safeDate;
+    window.location.href = 'pass.html?' + urlParameters;
+}
+
+// END FORM.HTML METHODS
+
+// PASS.HTML METHODS
+// Gets URL search parameters for pass.html
+function getURLData() {
+    const parameters = new URLSearchParams(document.location.search);
+    const studentName = parameters.get('name');
+    const studentDestination = parameters.get('destination');
+    const duration = parameters.get('duration');
+    const passDate = new Date(decodeURIComponent(parameters.get('date')));
+
+    const studentDate = passDate.toLocaleDateString();
+    const studentStart = new Date(passDate.getTime() + 60000);
+    studentEnd = new Date(studentStart.getTime() + duration*60000);
+
+    // Add parameters to HTML
+    document.getElementById('student-name').innerHTML = studentName;
+    document.getElementById('destination').innerHTML = studentDestination;
+    document.getElementById('date').innerHTML = studentDate;
+    document.getElementById('start').innerHTML = studentStart.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    document.getElementById('end').innerHTML = studentEnd.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    
+    updateBackgroundColor();
+    setInterval(updateBackgroundColor, 6000);
+}
+
+function updateBackgroundColor() {
+    
+    
+}
+
+function updateBackgroundColor() {
+    const date = new Date();
+    const timeRemaining = studentEnd - date;
+
+    // Update currentColor variable
+    if (timeRemaining < 0) {
+        currentColor = '#c80815'; // red
+    } else if (timeRemaining <= 120000) {
+        currentColor = '#ffc40c'; // yellow
+    } else {
+        currentColor = 'darkgreen';
     }
 
-    console.log(studentName);
-    console.log(studentDestination);
-    console.log(studentDuration);
+    // Apply Colors
+    document.getElementsByTagName('body')[0].style.backgroundColor = currentColor;
+    [...document.getElementsByClassName('color2')].forEach(element => {
+        element.style.color = currentColor;
+    });
 }
 
-function updateDuration() {
-    const duration = document.getElementById('duration-input').value;
-    const durationDisplay = document.getElementById('duration-display').innerHTML = duration + ' minutes';
-}
+// END PASS.HTML METHODS
